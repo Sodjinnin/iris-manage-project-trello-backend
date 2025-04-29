99fe07a0af14497f89a89ecb8d589f9c41f6b695
Sodjinnin <38305512+Sodjinnin@users.noreply.github.com>
2025-04-29 22:36:50 +0200

    chnage apitoken type
    chnage apitoken type

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -22,7 +22,7 @@ class User  implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column(length: 255)]
     private ?string $email = null;
 
-    #[ORM\Column(length: 255, nullable: true)]
+    #[ORM\Column(type: 'text', nullable: true)]
     private ?string $apiToken = null;
 
     #[ORM\Column(length: 255)]
